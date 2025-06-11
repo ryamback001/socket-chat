55d5ea82a147678ef522d9200f96903afa6ee711
Almeida <rian.almeida@ingrammicro.com>
2025-06-11 18:51:25 -0300

    alteracoes

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # socket-chat
 
 Aplicação socket
+
+Necessário ter instalado o Node.js.
+
+Para rodar execute os comandos:
+    1 - npm install 
+    2 - npm start
+    3 - abrir varias abas no browser
+    4 - enviar mensagem em uma aba e receber em todas as outras.
--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-const express = require('express')
-const socketIo = require('socket.io')
-const http = require('http')
-const path = require('path')
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server)
+const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-io.on('connection', (socket) =>{
-    console.log('Novo usuário conectado!')
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado!');
 
-    socket.on('chat message', (data) =>{
-        console.log(data)
-        io.emit('chat message', {user: data.user, message: data.message, time: data.time});
-        
-    })
+    socket.on('join room', (room) => {
+        socket.join(room);
+        console.log(`Usuário entrou na sala: ${room}`);
+    });
+
+    socket.on('leave room', (room) => {
+        socket.leave(room);
+        console.log(`Usuário saiu da sala: ${room}`);
+    });
+
+    socket.on('chat message', (data) => {
+        console.log(`[${data.room}] ${data.user}: ${data.message}`);
+        io.to(data.room).emit('chat message', {
+            user: data.user,
+            message: data.message,
+            time: data.time
+        });
+    });
 
     socket.on('disconnect', () => {
         console.log('Usuário desconectado');
-    })
-})
+    });
+});
 
-server.listen(3000, () =>{
-    console.log('Servidor rodando na porta 3000')
-})
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
